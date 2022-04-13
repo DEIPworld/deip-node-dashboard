02e410b991606a73796e90057d3580bda8b872e0
egortsaryk9 <egortsaryk@gmail.com>
2022-04-13 14:47:28 +0200

    Mainnet endpoint

--- packages/apps-config/src/endpoints/deip.ts
+++ packages/apps-config/src/endpoints/deip.ts
@@ -10,17 +10,17 @@ export function createDeip(t: TFunction): LinkOption[] {
   return [
     {
       isHeader: true,
-      text: t('rpc.deip', 'DEIP environment', { ns: 'apps-config' }),
+      text: t('rpc.deip', 'DEIP Mainnet', { ns: 'apps-config' }),
       textBy: '',
       value: ''
     },
     {
       info: 'deip',
       homepage: 'https://deip.world',
-      text: t('rpc.deip.endpoint', 'DEIP endpoint', { ns: 'apps-config' }),
-      textBy: "Coming soon",
+      text: t('rpc.deip.endpoint', 'DEIP Mainnet endpoint', { ns: 'apps-config' }),
+      textBy: "Octopus Network",
       // TODO: replace with Mainnet endpoint
-      value: ""
+      value: "wss://gateway.mainnet.octopus.network/deip/b9e1ipeh3ejw2znrb4s2xd4tlf6gynq0"
     }
   ]
 }
@@ -31,7 +31,7 @@ export function createTestnetDeip(t: TFunction): LinkOption[] {
   return [
     {
       isHeader: true,
-      text: t('rpc.deip.testnet', 'DEIP Testnet environment', { ns: 'apps-config' }),
+      text: t('rpc.deip.testnet', 'DEIP Testnet', { ns: 'apps-config' }),
       textBy: '',
       value: ''
     },
@@ -39,7 +39,7 @@ export function createTestnetDeip(t: TFunction): LinkOption[] {
       info: 'deip',
       homepage: 'https://deip.world',
       text: t('rpc.deip.testnet.endpoint', 'DEIP Testnet endpoint', { ns: 'apps-config' }),
-      textBy: "Octopus Network wss://gateway.testnet.octopus.network/deip/46v4p8ss613olf92p2scmsjud68mhzrr",
+      textBy: "Octopus Network",
       value: "wss://gateway.testnet.octopus.network/deip/46v4p8ss613olf92p2scmsjud68mhzrr"
     }
   ]
